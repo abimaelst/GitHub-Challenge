6a544c1979afc8518d0158681b9876732e4998c4
abimaelst <abimaelst@live.com>
2020-07-03 11:10:36 -0300

    Deploy Heroku

---
+++ build/precache-manifest.9a0595304f4d2eb56e90d0b7cac3403b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ff10aaa95f58f1f9a3ef26a3fe46c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8b03ac7b22730063c86",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "0b32c62c7e39be281916",
+    "url": "/static/css/main.464df1a8.chunk.css"
+  },
+  {
+    "revision": "c8b03ac7b22730063c86",
+    "url": "/static/js/2.80cd4b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.80cd4b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b32c62c7e39be281916",
+    "url": "/static/js/main.fddfa207.chunk.js"
+  },
+  {
+    "revision": "67e85333bb14618f3a58",
+    "url": "/static/js/runtime-main.d0c8e979.js"
+  }
+]);
